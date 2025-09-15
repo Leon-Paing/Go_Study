84c74f56084e4b884d363e06c579918d2256d216
Leon-Paing <yanpaingphyoe441@gmail.com>
2025-09-15 15:39:47 +0630

    added loop and exit

--- Day3/TodoCLI/todo.go
+++ Day3/TodoCLI/todo.go
@@ -8,37 +8,54 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to CLI Todo App")
+	fmt.Println("Welcome to CLI Todo App!\n")
 	options := []string{}
 	options = append(options, "Add", "List", "Mark as Done", "Delete", "Exit")
 
 	for index, option := range options {
-		fmt.Println(index, ")", option)
+		fmt.Println(index+1, ")", option, "\n")
 	}
 
 	// fmt.Println(options)
+	var List []TodoList
 	var input int
-	fmt.Print("Choose option:")
-	fmt.Scanln(&input)
-	fmt.Println("\nYou chose option", input, "\n")
+	loop := true
+	for loop == true {
+		fmt.Print("Choose option:")
+		fmt.Scanln(&input)
 
-	if input == 1 {
+		switch input {
+		case 1:
+			fmt.Print("Enter task:")
+			reader := bufio.NewReader(os.Stdin)
+			inputStr, _ := reader.ReadString('\n')
+			fmt.Println("\n")
+			inputStr = strings.TrimSpace(inputStr)
 
-		fmt.Print("Enter task:")
-		reader := bufio.NewReader(os.Stdin)
-		inputStr, _ := reader.ReadString('\n')
-		fmt.Println("\n")
-		inputStr = strings.TrimSpace(inputStr)
+			var todo TodoList
+			result, status := todo.addTodo(inputStr)
+			task := TodoList{Task: result, Status: status}
+			List = append(List, task)
+			fmt.Println("Todo added!\n")
+		case 2:
+			fmt.Println("\n----Todo List----\n")
+			if len(List) == 0 {
+				fmt.Println("No tasks to do! \n")
+				break
+			} else {
+				for i, todo := range List {
+					fmt.Println("[", i, "]", todo.Task, "[", statusChecker(todo.Status), "]\n")
+				}
+			}
+		case 5:
+			fmt.Println("See you! Good Bye!")
+			loop = false
 
-		result, bool := addTodo(inputStr)
-		fmt.Println("Todo added!\n")
-		fmt.Println("----Todo List----\n")
-		if bool == false {
-			fmt.Println("[1]:", result, "(Pending)")
-		} else {
-			fmt.Println("[1]", result, "(Done)")
+		default:
+			fmt.Println("This is default")
 		}
 	}
+	fmt.Println("-------End-------")
 }
 
 type TodoList struct {
@@ -46,10 +63,17 @@ type TodoList struct {
 	Status bool
 }
 
-func addTodo(task string) (string, bool) {
-	t := new(TodoList)
+func (t *TodoList) addTodo(task string) (string, bool) {
 	t.Task = task
-	t.Status = true
+	t.Status = false
 
 	return t.Task, t.Status
+}
+
+func statusChecker(stat bool) string {
+	if stat == false {
+		return "Pending"
+	} else {
+		return "Done"
+	}
 }
