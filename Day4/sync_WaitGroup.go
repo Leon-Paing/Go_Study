03bcf7c5960dde17741e9e4f0598cd6389848e17
Leon-Paing <yanpaingphyoe441@gmail.com>
2025-09-16 14:33:25 +0630

    added sync.WaitGroup lesson

---
+++ Day4/sync_WaitGroup.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 4; i++ {
+		wg.Add(1)
+		go worker(i, &wg)
+	}
+
+	wg.Wait()
+	fmt.Println("All workers done! Exiting....")
+	time.Sleep(1 * time.Second)
+	fmt.Println("\nClosed Program!")
+}
+
+func worker(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Worker", id, "starting...")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Worker", id, "Done!")
+}
