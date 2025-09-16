6e343daa513e31970b87d250f741138c33688d37
Leon-Paing <yanpaingphyoe441@gmail.com>
2025-09-16 14:12:11 +0630

    added select channel concept

---
+++ Day4/selectChannel.go
@@ -0,0 +1,33 @@
+// Wait for whichever channel is ready.
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch1 <- "Hello from channel 1"
+	}()
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		ch2 <- "Hello from channel 2"
+	}()
+
+	for i := 0; i <= 2; i++ {
+		select {
+		case msg1 := <-ch1:
+			fmt.Println("Message:", msg1)
+		case msg2 := <-ch2:
+			fmt.Println("Message:", msg2)
+		case <-time.After(3 * time.Second):
+			fmt.Println("Timeout!..Finished")
+		}
+	}
+}
