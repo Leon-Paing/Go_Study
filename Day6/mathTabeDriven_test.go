2675a210944f7246f88e015b9a1a0c1f8d29050b
Leon-Paing <yanpaingphyoe441@gmail.com>
2025-09-18 22:15:35 +0630

    added table driven test

---
+++ Day6/mathTabeDriven_test.go
@@ -0,0 +1,24 @@
+package mathutils
+
+import "testing"
+
+func TestAdd_TableDriven(t *testing.T) {
+	tests := []struct {
+		name     string
+		a, b     int
+		expected int
+	}{
+		{"2+2", 2, 2, 6},
+		{"-1+1", -1, 1, 0},
+		{"54+34", 54, 34, 88},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := Add(tt.a, tt.b)
+			if result != tt.expected {
+				t.Errorf("Expected %d got %d", tt.expected, result)
+			}
+		})
+	}
+}
