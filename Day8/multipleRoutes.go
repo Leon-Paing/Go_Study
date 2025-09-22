7f96a88bbe16e76503c0d6f5aaa0269d71b762f8
Leon-Paing <yanpaingphyoe441@gmail.com>
2025-09-22 21:19:59 +0630

    learned multiple route handler

---
+++ Day8/multipleRoutes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/about", AboutPage)
+
+	fmt.Println("Server running at port 8834...")
+	http.ListenAndServe(":8834", nil)
+}
+
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from Home!")
+}
+
+func AboutPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is About Page!")
+}
