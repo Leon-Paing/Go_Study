8ff54e658b443f859c7a65250b355c147239e82f
Leon-Paing <yanpaingphyoe441@gmail.com>
2025-09-26 22:29:06 +0630

    added fetch by id

--- Day12/simpleTodoGETPOST_API.go
+++ Day12/simpleTodoGETPOST_API.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -26,14 +27,29 @@ func main() {
 	e.POST("/todos", func(c echo.Context) error {
 		var newTodo Todo
 		if err := c.Bind(&newTodo); err != nil {
-			return c.JSON(http.StatusBadRequest, map[string]string{
-				"error": "Bad Request",
+			return c.JSON(http.StatusBadGateway, map[string]string{
+				"error": "Not Available",
 			})
 		}
 
 		newTodo.ID = len(todos) + 1
 		todos = append(todos, newTodo)
 		return c.JSON(http.StatusCreated, newTodo)
+	})
+
+	e.GET("/todo/:id", func(c echo.Context) error {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, "Invalid ID")
+		}
+		for _, todo := range todos {
+			if todo.ID == id {
+				return c.JSON(http.StatusOK, todo)
+			}
+		}
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "Data Not Found!",
+		})
 	})
 
 	e.Logger.Fatal(e.Start(":8834"))
