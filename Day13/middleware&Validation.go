93022024522a5dde82b30136a0b14bf5cce13fa8
Leon-Paing <yanpaingphyoe441@gmail.com>
2025-09-28 20:00:48 +0630

    added main func

--- Day13/middleware&Validation.go
+++ Day13/middleware&Validation.go
@@ -1,11 +1,18 @@
 package main
 
+
 type Todo struct {
 	ID   int    `json:"id"`
 	Task string `json:"task" validate:"required,min=3"`
 }
 
-var todo = []Todo{
+var todos = []Todo{
 	{ID: 1, Task: "Learn Validation"},
 	{ID: 1, Task: "Add Middleware"},
+}
+
+
+
+func main{
+	
 }
