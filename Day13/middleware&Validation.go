ecf84cc7d984699931754820450954ca265a2e71
Leon-Paing <yanpaingphyoe441@gmail.com>
2025-09-28 20:00:13 +0630

    added initial Values

--- Day13/middleware&Validation.go
+++ Day13/middleware&Validation.go
@@ -4,3 +4,8 @@ type Todo struct {
 	ID   int    `json:"id"`
 	Task string `json:"task" validate:"required,min=3"`
 }
+
+var todo = []Todo{
+	{ID: 1, Task: "Learn Validation"},
+	{ID: 1, Task: "Add Middleware"},
+}
