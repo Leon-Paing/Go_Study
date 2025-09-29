61ad857ebde39ff8043f82b5db4c127ca7933314
Leon-Paing <yanpaingphyoe441@gmail.com>
2025-09-29 22:15:28 +0630

    added Day14 and initialized module and wrote get all API

---
+++ Day14/todoCRUD_Api.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+)
+
+type Todo struct {
+	ID     int    `json:"id"`
+	Task   string `json:"task" validate:"required,min=3"`
+	Status bool   `json:"status"`
+}
+
+var todos = []Todo{
+	{ID: 1, Task: "Write API", Status: true},
+	{ID: 2, Task: "Add Validation", Status: false},
+	{ID: 3, Task: "Test API", Status: false},
+}
+
+var validate = validator.New()
+
+func main() {
+	e := echo.New()
+	// e.HTTPErrorHandler = customErrorHander
+
+	e.GET("/todos", ListTodos)
+	// e.GET("/todos/:id", GetTodo)
+	// e.POST("/todos", AddTodo)
+	// e.PUT("/todos/:id", UpdateTodo)
+	// e.DELETE("todos/:id", DeleteTodo)
+
+	e.Logger.Fatal(e.Start(":8834"))
+}
+
+func ListTodos(c echo.Context) error {
+	return c.JSON(http.StatusOK, todos)
+}
